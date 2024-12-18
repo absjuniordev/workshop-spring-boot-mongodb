ab0c9735c98ad841c07a035e2e6990b17618861a
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-17 22:16:03 -0300

    DTO to return users

--- src/main/java/com/absjuniordev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/absjuniordev/workshopmongo/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.absjuniordev.workshopmongo.domain.User;
+import com.absjuniordev.workshopmongo.dto.UserDTO;
 import com.absjuniordev.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +20,9 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).toList();
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
