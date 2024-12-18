2d560e9e61998c1e5e6ac621885fc211d24651ec
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-17 22:45:05 -0300

    Get user by Id

---
+++ src/main/java/com/absjuniordev/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.absjuniordev.workshopmongo.resources.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.absjuniordev.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandarError> objectNotfaund(ObjectNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandarError err = new StandarError(System.currentTimeMillis(), status.value(), "Não encontrado",
+				e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
--- src/main/java/com/absjuniordev/workshopmongo/services/UserService.java
+++ src/main/java/com/absjuniordev/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.absjuniordev.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.absjuniordev.workshopmongo.domain.User;
 import com.absjuniordev.workshopmongo.repository.UserRepository;
+import com.absjuniordev.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
 	}
 }
