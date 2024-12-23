67f49fd1089c0ca2726c5d0cdccef75f24ab7645
Arnaldo Junior <abs.junnior@hotmail.com>
2024-12-23 11:28:10 -0300

    Post referencie

--- src/main/java/com/absjuniordev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/absjuniordev/workshopmongo/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.absjuniordev.workshopmongo.domain.Post;
 import com.absjuniordev.workshopmongo.domain.User;
 import com.absjuniordev.workshopmongo.dto.UserDTO;
 import com.absjuniordev.workshopmongo.services.UserService;
@@ -61,4 +62,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+	
 }
